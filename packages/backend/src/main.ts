6a2079be7f0784e81b405c6dd99b5493c67c5638
Damian Kolenda <damiankolenda7@gmail.com>
2020-02-02 23:49:46 +0100

    configuration swagger documentation

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const options = new DocumentBuilder()
+  .setTitle('MyNetflix Api')
+  .setDescription('API description')
+  .setVersion('1.0')
+  .build();
+
+const document = SwaggerModule.createDocument(app, options);
+SwaggerModule.setup('api', app, document);
+
   await app.listen(3300);
 
   if (module.hot) {
