e2ec3c845f368b7a1a176834ee71560e88be78da
Damian Kolenda <damiankolenda7@gmail.com>
2020-02-05 01:43:52 +0100

    WF-40_swagger

--- packages/backend/src/app.controller.ts
+++ packages/backend/src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get,Param,Query} from '@nestjs/common';
 import { AppService } from './app.service';
+
+
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get("test/:id")
+  getHello(@Param('id') id:string, @Query("test1") zbys:number): string {
     return this.appService.getHello();
   }
 }
--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 import { Logger } from '@nestjs/common';
@@ -8,13 +9,23 @@ const port = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const options = new DocumentBuilder()
+  .setTitle('MyNetflix Api')
+  .setDescription('API description')
+  .setVersion('1.0')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   Logger.log(`Server running on http://localhost:${port}`,'Bootstrap');
 
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
-    }
+  }
 }
   
 
