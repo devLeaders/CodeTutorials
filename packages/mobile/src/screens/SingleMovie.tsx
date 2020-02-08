4f9ee7f393217c3b15bb95f38c3b780b2bd21d06
mbobas <michal.gulczynski88@gmail.com>
2020-02-08 14:45:01 +0100

    step1

--- packages/mobile/src/screens/SingleMovie.tsx
+++ packages/mobile/src/screens/SingleMovie.tsx
@@ -8,7 +8,9 @@ import {
   Button,
   StatusBar,
   FlatList,
+  Image,
 } from 'react-native';
+import player from '../../assets/img/player.jpg';
 
 //style do pliku const
 
@@ -21,21 +23,33 @@ const SingleMovie = () => {
         <Text>Searchbar - Header</Text>
       </View>
       <View style={styles.player}>
-        <Text>Player</Text>
+        <Image style={styles.playerimage} source={player} />
       </View>
-      <View style={styles.imagelist}>
-        <Text>List of pictures</Text>
-        <Text>List of pictures</Text>
-        <Text>List of pictures</Text>
-        <Text>List of pictures</Text>
+      <Button
+        style={styles.playbutton}
+        title='PLAY'
+        onPress={()=>{console.log('PLAYbutton')}}
+        
+      />
+      <View style={styles.imageList}>
+      <Image style={styles.imageListItem} source={player} />
+      <Image style={styles.imageListItem} source={player} />
+      <Image style={styles.imageListItem} source={player} />
+      <Image style={styles.imageListItem} source={player} />
       </View>
+
       <View style={styles.description}>
-        <Text>Description</Text>
+        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar sic tempor.
+        Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus pronin sapien nunc accuan eget.</Text>
       </View>
       <View style={styles.footer}>
         <Text>Footer1</Text>
-      </View>
+        <Text>Footer1</Text>
+        <Text>Footer1</Text>
+        <Text>Footer1</Text>
+        <Text>Footer1</Text>
 
+      </View>
       </View>
     </ScrollView>
   );
@@ -50,25 +64,39 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   header: {
-    justifyContent: 'center',
-    flexDirection: 'column',
-  },
-  player: {
-    justifyContent: 'center',
-    flexDirection: 'column',
-  },
-  imagelist: {
-    justifyContent: 'flex-start',
-    flexDirection: 'row',
   },
   description: {
-    justifyContent: 'flex-start',
-    flexDirection: 'column',
   },
   footer: {
-    justifyContent: 'center',
-    flexDirection: 'column',
   },
+  player: {
+  },
+  playerimage: {
+    height: 250,
+    width: 420,
+  },
+  imageList: {
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    marginLeft: 100,
+
+  },
+  imageListItem: {
+    height: 100,
+    width: 100,
+    margin: 10,
+    borderRadius: 4,
+  },
+  playbutton: {
+    borderWidth:1,
+    borderColor:'rgba(0,0,0,0.2)',
+    alignItems:'center',
+    justifyContent:'center',
+    width:100,
+    height:100,
+    backgroundColor:'#fff',
+    borderRadius:50,
+  }
 });
 
 export default SingleMovie;
