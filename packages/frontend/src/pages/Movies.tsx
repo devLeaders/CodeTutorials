67e29e946e2b68d96bace2f107e0e5969fb45847
duch0416 <mikolajduchewicz@gmail.com>
2020-02-11 14:55:33 +0100

    modify Movies component

--- packages/frontend/src/pages/Movies.tsx
+++ packages/frontend/src/pages/Movies.tsx
@@ -3,30 +3,43 @@ import MoviesTypes from "../pages/MoviesTypes.d";
 import MovieNavBar from "../components/MovieNavBar";
 import styled from "styled-components";
 import FiltrationBtns from "../components/FiltrationBtns";
+import MovieSection from "../components/MovieSection";
+import Colors from "../constans/Colors";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  background-color: #222;
+  width: 100%;
+`;
+
+const NewContent = styled.div`
+  height: 60%;
+  width: 100%;
+  background-color: red;
+`;
+
+const Section = styled.section`
+  margin-top: 15px;
+  height: 35%;
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+`;
 
 export default class Movies extends React.Component<any, MoviesTypes> {
   render() {
     return (
       <Wrapper>
         <MovieNavBar></MovieNavBar>
-        <NewContent />
-        <FiltrationBtns></FiltrationBtns>
+        <Section>
+          <NewContent />
+          <FiltrationBtns></FiltrationBtns>
+        </Section>
+        <MovieSection title="Polecane dla ciebie"></MovieSection>
       </Wrapper>
     );
   }
 }
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  min-height: 100vh;
-  width: 100%;
-`;
-
-const NewContent = styled.div`
-  margin-top: 30px;
-  height: 150px;
-  width: 90%;
-  background-color: red;
-`;
