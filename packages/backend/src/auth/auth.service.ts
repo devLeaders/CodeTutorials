ba2b3a8bd750dcd0f01a941ef1197acb41bb5191
unknown <piotr.najecki@gmail.com>
2020-02-17 15:40:06 +0100

    SignUp First Commit

---
+++ packages/backend/src/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { UsersRepository } from './user.repository';
+import { UserDTO } from './user.dto';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+
+@Injectable()
+export class AuthService {
+    constructor(
+        @InjectRepository(UsersRepository) private usersRepository: UsersRepository,
+    ){}
+
+    async signUp(userDTO: UserDTO): Promise<void>{
+        return this.usersRepository.signUp(userDTO);           
+    }
+}
---
+++ packages/backend/src/auth/user.repository.ts
@@ -0,0 +1,30 @@
+import { UserDTO } from './user.dto';
+import { Repository, EntityRepository, AdvancedConsoleLogger } from 'typeorm';
+import {UserEntity} from './user.entity';
+import {ConflictException, InternalServerErrorException} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
+@EntityRepository(UserEntity)
+export class UsersRepository extends Repository<UserEntity> {
+    async signUp(userDTO: UserDTO): Promise<void> {               
+        
+        const user = new UserEntity();        
+        user.email = userDTO.email;
+        user.salt = await bcrypt.genSalt();
+        user.password = await this.hashPassword(userDTO.password, user.salt);        
+
+        try {
+        await user.save();
+        } catch(error) {
+            if (error.code === '23505')  // duplicate email/username
+            {throw new ConflictException("Given email already exists in database");
+             }  else {
+            throw new InternalServerErrorException();
+            }
+        }
+    }
+
+    private async hashPassword(password: string, salt: string): Promise<string> {
+        return bcrypt.hash(password, salt);
+    }
+}
