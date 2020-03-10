75fdcb5244bd9655bf58dd39490838be9d8fd476
duch0416 <mikolajduchewicz@gmail.com>
2020-03-10 15:42:58 +0100

    add videoStream endpoint

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -6,13 +6,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { VideosModule } from './videos/videos.module';
 import { configService } from './config/config.service';
 import { FilesModule } from './files/files.module';
+import { VideoStreamModule } from './video-stream/video-stream.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
     VideosModule,
     FilesModule,
-    AuthModule
+    AuthModule,
+    VideoStreamModule
   ],
   controllers: [AppController],
   providers: [AppService],
