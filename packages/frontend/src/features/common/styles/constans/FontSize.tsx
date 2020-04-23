9709c31d061d07cc33ba00ecd03022858b3ac274
duch0416 <mikolajduchewicz@gmail.com>
2020-04-23 20:56:53 +0200

    video description component merge

--- packages/frontend/src/features/common/styles/constans/FontSize.tsx
+++ packages/frontend/src/features/common/styles/constans/FontSize.tsx
@@ -1,4 +1,11 @@
 export enum FontSize {
+
+    VERY_SMALL = '9px',
+    small="13px",
+    SMALL = '15px',
+    NORMAL = '16px',
+    MEDIUM = '20px',
+    BIG = '30px',
     paragraphDesktop = '18px',
     paragraphMobile = '16px',
     headerDesktop = '32px',
--- packages/frontend/src/features/common/styles/constans/fontWeight.tsx
+++ packages/frontend/src/features/common/styles/constans/fontWeight.tsx
@@ -2,6 +2,7 @@ export enum fontWeight {
     light = 300,
     regular = 400,
     medium = 500,
+    SEMI_BOLD = 600,
     bold = 700,
     black = 900,
 }
