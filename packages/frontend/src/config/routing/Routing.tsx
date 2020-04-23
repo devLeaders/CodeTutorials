8fa3bb8edd050842159917a04b53efec234b2626
duch0416 <mikolajduchewicz@gmail.com>
2020-04-23 20:57:40 +0200

    change home  routing

--- packages/frontend/src/config/routing/Routing.tsx
+++ packages/frontend/src/config/routing/Routing.tsx
@@ -4,14 +4,14 @@ import { Route, Switch } from "react-router-dom";
 import NAVIGATION from "./NavigationPath";
 import SingleMoviePage from "../../features/videos/page/SingleMoviePage";
 import Movies from "../../features/videos/page/Movies";
-import VideoDescription from "../../features/videos/components/singleVideoComponents/VideoDescription"
+
 
 const Routing: React.FC = () => {
   return (
     <Switch>
       <Route path={`${NAVIGATION.MOVIES}/:id`} exact component={SingleMoviePage} />
       <Route path={NAVIGATION.MOVIES} exact component={Movies} />
-      <Route component={VideoDescription} />
+      <Route component={Movies} />
     </Switch>
   );
 };
