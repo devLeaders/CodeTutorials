d246f712aef6b935212d545c426dbd1c1033fd3b
duch0416 <mikolajduchewicz@gmail.com>
2020-05-05 16:36:42 +0200

    fix routing after merge

--- packages/frontend/src/config/routing/Routing.tsx
+++ packages/frontend/src/config/routing/Routing.tsx
@@ -3,14 +3,13 @@ import { Route, Switch } from "react-router-dom";
 import NAVIGATION from "./NavigationPath";
 import SingleMoviePage from "../../features/videos/page/SingleMoviePage";
 import Movies from "../../features/videos/page/Movies";
-import NavBarContainer from "../../features/common/components/layout/navBar/NavBarContainer"
 
 const Routing: React.FC = () => {
   return (
     <Switch>
       <Route path={`${NAVIGATION.MOVIES}/:id`} exact component={SingleMoviePage} />
       <Route path={NAVIGATION.MOVIES} exact component={Movies} />
-      <Route component={NavBarContainer   } />
+      <Route component={Movies} />
     </Switch>
   );
 };
