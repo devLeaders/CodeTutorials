cf00ac4ac38c0284c69c9ab59c11824757bdae50
mbobas <michal.gulczynski88@gmail.com>
2020-05-25 18:14:25 +0200

    afterCRMainHeader1

--- packages/mobile/src/features/videos/components/MainScreen/SmallHeaderButton copy.tsx
+++ packages/mobile/src/features/videos/components/MainScreen/SmallHeaderButton copy.tsx
@@ -1,33 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 import { FontWeight } from '../../../common/styles/constans/FontWeight';
-import { FontFamily } from '../../../common/styles/constans/FontFamily';
 import { FontSize } from '../../../common/styles/constans/FontSize';
 import { Color } from '../../../common/styles/constans/Color';
 import { MyText } from '../../../common/styles/MyText';
-
-const Wrapper = styled.TouchableOpacity`
-`;
+import BigHeaderButton from './BigHeaderButton';
 
 const ButtonFilter = styled.TouchableOpacity({
     //marginRight: 10,
     height: 26,
     minWidth: 51,
-    //flexDirection: 'row',
-    alignItems: 'center',
     justifyContent: 'center',
-    borderRadius: 10,
-    borderColor: Color.LIGHTGREYXX,
-    //border: 1,  ??
-    backgroundColor: Color.WHITE,
-    shadowColor: Color.LIGHTGREYX,
-    shadowOffset: {
-        width: 0,
-        height: 2,
-    },
-    shadowOpacity: "1",
-    shadowRadius: 8,
-    elevation: "3",
   });
 
   const TextButton = styled(MyText)`
@@ -35,28 +18,15 @@ const ButtonFilter = styled.TouchableOpacity({
     font-weight: ${FontWeight.LIGHT};
     text-align: left;
     min-width: 26px;
-    font-family: ${FontFamily.GLOBAL_MONT};
-    color: ${Color.BLACK};
-    justify-content: space-around;
 `;
 
   const ImageButton = styled.Image`
+    margin-left: 0px;
     height: 10px;
     width: 10px;
-     
 `;
-  
 
-interface SmallHeaderButtonProps {
-    navigation: any,
-    text: string,
-    image: string,
-    //onSubmitFilter(): void,
-  }
-  
-  //wybór white button | blackbutton
-
-export default class SmallHeaderButton extends React.Component<SmallHeaderButtonProps> {
+export default class SmallHeaderButton2 extends BigHeaderButton {
     navi = (navigation) => {
         this.props.navigation.navigate(this.props.goto)
     
@@ -70,7 +40,6 @@ export default class SmallHeaderButton extends React.Component<SmallHeaderButton
                 <TextButton>{this.props.text}</TextButton>
                 </ButtonFilter>
             </Wrapper>
-
         );
     }
 }
