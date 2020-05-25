444dd6e1f8bda7f74ed47892444fbd2eadeda441
duch0416 <mikolajduchewicz@gmail.com>
2020-05-25 12:23:04 +0200

    cosmetic changes

--- packages/frontend/src/features/filters/components/Categories.tsx
+++ packages/frontend/src/features/filters/components/Categories.tsx
@@ -1,38 +1,49 @@
-import * as React from 'react';
-import styled from "styled-components"
+import * as React from "react";
+import styled from "styled-components";
 
-import {FontSize} from "../../common/styles/constans/FontSize"
-import {fontWeight} from "../../common/styles/constans/fontWeight"
-import {Device} from "../../common/styles/constans/Device";
-import CategorieBtn from "./CategorieBtn"
+import { FontSize } from "../../common/styles/constans/FontSize";
+import { fontWeight } from "../../common/styles/constans/fontWeight";
+import { Device } from "../../common/styles/constans/Device";
+import CategorieBtn from "./CategorieBtn";
 
 const BtnWrapper = styled.div`
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    margin-top: 21px;
-    @media ${Device.tablet}{
-        width: 334px;
-    }
-`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin-top: 21px;
+  @media ${Device.tablet} {
+    width: 334px;
+  }
+`;
 
 const Title = styled.h3`
-    margin-top: 35px;
-    font-size: ${FontSize.MEDIUM};
-    font-weight: ${fontWeight.SEMI_BOLD};
-`
+  margin-top: 35px;
+  font-size: ${FontSize.MEDIUM};
+  font-weight: ${fontWeight.SEMI_BOLD};
+`;
 
-const categories = ["UX/UI Design", "Graphic Design", "Computer Science Engineer", "Movies", "Web Developer", "Dance", "DJ", "Guitarist"]
+const categories = [
+  "UX/UI Design",
+  "Graphic Design",
+  "Computer Science Engineer",
+  "Movies",
+  "Web Developer",
+  "Dance",
+  "DJ",
+  "Guitarist",
+];
 
 const Categories: React.SFC = () => {
-    return ( 
-        <>
-        <Title>Kategorie</Title>
-        <BtnWrapper>
-            {categories.map((categorie: string) => <CategorieBtn key={categorie} categorie={categorie}/>)}
-        </BtnWrapper>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <Title>Kategorie</Title>
+      <BtnWrapper>
+        {categories.map((categorie: string) => (
+          <CategorieBtn key={categorie} categorie={categorie} />
+        ))}
+      </BtnWrapper>
+    </>
+  );
+};
+
 export default Categories;
