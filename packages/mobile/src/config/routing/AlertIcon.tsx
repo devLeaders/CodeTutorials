66c50f9f03b9e369a80cc4740dc9b90f0244f1b2
Justyna Kolenda <justynakolenda@MacBook-Pro-Justyna.local>
2020-07-16 15:21:09 +0200

    Resolv CR

---
+++ packages/mobile/src/config/routing/AlertIcon.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { AlertV, AlertIc, AlertView, AlertText } from "../../features/common/components/TabBottomNavStyle";
+
+export default class AlertIcon extends React.Component {
+    render(){
+        return(
+            <AlertV>
+                <AlertIc source={{uri:'ic_alert'}}/>
+                <AlertView>
+                    <AlertText>2</AlertText>
+                </AlertView>
+            </AlertV>
+        )
+    }
+}
