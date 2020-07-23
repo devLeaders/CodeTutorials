9c02d3276db1b4ac75073b58fec2a5d3f98ec978
mbobas <michal.gulczynski88@gmail.com>
2020-07-23 21:04:42 +0200

    FlatListAdded

--- packages/mobile/src/features/videos/page/FilterWithList.tsx
+++ packages/mobile/src/features/videos/page/FilterWithList.tsx
@@ -3,9 +3,16 @@ import {View, ScrollView, Text} from 'react-native';
 import styled from 'styled-components/native';
 import ShortcutToFilters from '../../videos/components/FiltersScreen/ShortcutToFilters';
 import { NavigationName } from '../../../config/routing/NavigationName';
+import { Color } from '../../../features/common/styles/constans/Color';
+import { SlaiderNormal } from '../components/Movies/SlaiderNormal';
 
 const Wrapper = styled.View({
   flex: 1,
+});
+
+const ListOfMovies = styled.ScrollView({
+  flex: 3,
+  backgroundColor: Color.WHITE,
 });
 
 interface FilterWithListProps {
@@ -28,6 +35,10 @@ render() {
         serachitem="Docker podstawy"
         onSubmitSearch={() => this.props.navigation.navigate(NavigationName.HOME)}
         />
+      <ListOfMovies>
+        <Text>List of Movies</Text>
+        <SlaiderNormal></SlaiderNormal>
+      </ListOfMovies>
     </Wrapper>
   )};
 };
