6c215ca5599d3507ef0282416190aaf64d84cec0
Adamwojty <adam12573@gmail.com>
2020-07-28 07:39:18 +0200

    SignIn and SignUp Views

--- packages/frontend/src/config/axios/configAxios.tsx
+++ packages/frontend/src/config/axios/configAxios.tsx
@@ -1,13 +1,13 @@
-import axios from 'axios'
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3300',
-    timeout: 1000,
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        "Authorization": `Bearer ${localStorage.getItem("token")}`
-    }
+  baseURL: "http://localhost:3300",
+  timeout: 1000,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  },
 });
 
-export default instance
+export default instance;
--- packages/frontend/src/features/auth/components/ProtectedRoute.tsx
+++ packages/frontend/src/features/auth/components/ProtectedRoute.tsx
@@ -1,29 +1,29 @@
 import React, { Component } from "react";
 import { Route, Redirect } from "react-router-dom";
-import { loginSubmit } from "../action/authAction"
-import {Navigation} from '../../../config/routing/NavigationPath'
+import { loginSubmit } from "../action/authAction";
+import { Navigation } from "../../../config/routing/NavigationPath";
 
-export const ProtectedRoute = ({ component: Component, ...rest }:any) => {
-    const checkToken = localStorage.getItem('token')
-    return (
-      <Route
-        {...rest}
-        render={props => {
-          if (checkToken) {
-            return <Component {...props} />;
-          } else {
-            return (
-              <Redirect
-                to={{
-                    pathname: Navigation.LOGIN,
-                    state: {
-                    from: props.location
-                  }
-                }}
-              />
-            );
-          }
-        }}
-      />
-    );
-  };
+export const ProtectedRoute = ({ component: Component, ...rest }: any) => {
+  const checkToken = localStorage.getItem("token");
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (checkToken) {
+          return <Component {...props} />;
+        } else {
+          return (
+            <Redirect
+              to={{
+                pathname: Navigation.LOGIN,
+                state: {
+                  from: props.location,
+                },
+              }}
+            />
+          );
+        }
+      }}
+    />
+  );
+};
