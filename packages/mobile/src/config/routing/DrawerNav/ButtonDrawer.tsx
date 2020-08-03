0ce7457d9d4614f33af0e82f504030a6e9d8c755
Justyna Kolenda <justynakolenda@MacBook-Pro-Justyna.local>
2020-08-03 14:55:21 +0200

    styled drawer navigation

--- packages/mobile/src/config/routing/DrawerNav/ButtonDrawer.tsx
+++ packages/mobile/src/config/routing/DrawerNav/ButtonDrawer.tsx
@@ -1,52 +1,8 @@
 import React from 'react';
-import styled from 'styled-components/native';
-import { FontWeight } from '../../../features/common/styles/constans/FontWeight';
-import { FontSize } from '../../../features/common/styles/constans/FontSize';
-import { Color } from '../../../features/common/styles/constans/Color';
 import { NavigationName } from '../NavigationName';
-import {TouchableOpacity, Image} from 'react-native';
-import { MyText } from '../../../features/common/styles/MyText';
 import { useNavigation } from '@react-navigation/native';
+import { Wrapper, ButtonDraw, IcDraw, TextButton } from './DrawerStyle';
 
-const Wrapper = styled.TouchableOpacity({
-    flex: 1,
-});
-
-const ButtonDraw = styled.TouchableOpacity({
-    marginRight: 16,
-    marginLeft: 16,
-    marginBottom: 25,
-    height: 37,
-    width: 224,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    borderRadius: 10,
-    borderColor: Color.LIGHTGREYX,
-    backgroundColor: Color.WHITE,
-    shadowColor: Color.LIGHTGREYX,
-    shadowOffset: {
-        width: 0,
-        height: 2,
-    },
-    shadowOpacity: "1",
-    shadowRadius: 8,
-    elevation: "3",
-  });
-
-  const TextButton = styled(MyText)({
-    fontSize: FontSize.NORMAL,
-    fontWeight: parseInt(FontWeight.BOLD, 10),
-    color: Color.BLACK,
-  });
-
-  const IcDraw = styled.Image({
-    width: 26,
-    height: 26,
-    marginRight: 9,
-    marginLeft: 19
-  });
-  
 
 interface ButtonDrawerP {
     text: string,
--- packages/mobile/src/config/routing/DrawerNav/DrawerContentScreen.tsx
+++ packages/mobile/src/config/routing/DrawerNav/DrawerContentScreen.tsx
@@ -7,6 +7,8 @@ import {
 import { NavigationName } from '../NavigationName';
 import ButtonDrawer from './ButtonDrawer';
 import { Color } from '../../../features/common/styles/constans/Color';
+import { DrawerWraper, TitleProfile, WrapWidth } from './DrawerStyle';
+import ProfileBox from './ProfileBox';
 
 export function DrawerContent(props) {
 
@@ -14,28 +16,39 @@ export function DrawerContent(props) {
     return(
         <View style={{flex:1,  backgroundColor: Color.WHITE}}>
             <DrawerContentScrollView {...props}>
-                <View style={styles.drawerContent}>
-                    <ButtonDrawer 
-                        goto={NavigationName.MOVIELIST}
-                        text='Edytuj profil'
-                        icon='drawer_ic'
-                    />
-                    <ButtonDrawer 
-                        goto={NavigationName.MOVIELIST}
-                        text='Abonament i Faktury'
-                        icon='drawer_ic'
-                    />
-                    <ButtonDrawer 
-                        goto={NavigationName.MOVIELIST}
-                        text='Obejrzane Filmy'
-                        icon='drawer_ic'
-                    />
-                    <ButtonDrawer 
-                        goto={NavigationName.MOVIELIST}
-                        text='Admin Panel'
-                        icon='drawer_ic'
-                    />
-                </View>
+                <DrawerWraper>
+                    <WrapWidth>
+                        <TitleProfile>Profil</TitleProfile>
+                        <ProfileBox 
+                            name={'Sebastian'}
+                            icon={'useric'}
+                            mail={'seba@gmail.com'}
+                            userType={'Mentor'}
+                        />
+                    </WrapWidth>
+                    <WrapWidth>
+                        <ButtonDrawer 
+                            goto={NavigationName.MOVIELIST}
+                            text='Edytuj profil'
+                            icon='drawer_ic'
+                        />
+                        <ButtonDrawer 
+                            goto={NavigationName.MOVIELIST}
+                            text='Abonament i Faktury'
+                            icon='drawer_ic'
+                        />
+                        <ButtonDrawer 
+                            goto={NavigationName.MOVIELIST}
+                            text='Obejrzane Filmy'
+                            icon='drawer_ic'
+                        />
+                        <ButtonDrawer 
+                            goto={NavigationName.MOVIELIST}
+                            text='Admin Panel'
+                            icon='drawer_ic'
+                        />
+                </WrapWidth>
+                </DrawerWraper>
             </DrawerContentScrollView>
         </View>
     );
