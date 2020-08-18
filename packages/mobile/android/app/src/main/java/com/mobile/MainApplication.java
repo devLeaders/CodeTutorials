01fb57304d89ee33b2c5f3e0eca5e25a1400023c
Justyna Kolenda <justynakolenda@MacBook-Pro-Justyna.local>
2020-08-18 11:46:08 +0200

    up

--- packages/mobile/android/app/src/main/java/com/mobile/MainApplication.java
+++ packages/mobile/android/app/src/main/java/com/mobile/MainApplication.java
@@ -27,7 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          new MainReactPackage(),
+          // new MainReactPackage(),
           new ReactNativeFirebaseMessagingPackage();
           return packages;
         }
