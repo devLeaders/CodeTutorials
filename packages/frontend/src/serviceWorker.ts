f7e0b6f5215944719b3d5ab69e3e5c4424c08349
Adamwojty <adam12573@gmail.com>
2020-08-20 18:41:18 +0200

    FCM SW fix

--- packages/frontend/src/serviceWorker.ts
+++ packages/frontend/src/serviceWorker.ts
@@ -1,5 +1,4 @@
 import fb from "./config/firebase/firebaseInit";
-import { postRegistrationToken } from "@project/common/features/notifications/connectors";
 
 const messaging = fb.messaging();
 messaging.usePublicVapidKey(
@@ -11,10 +10,6 @@ const registerToken = async () => {
     await Notification.requestPermission();
     const registrationToken = await messaging.getToken();
     await localStorage.setItem("notification-token", registrationToken);
-    await postRegistrationToken({
-      userVisibleOnly: true,
-      registrationToken: registrationToken,
-    });
   } catch (err) {
     // window.location.reload();
   }
@@ -25,17 +20,12 @@ const tokenRefresh = () => {
       .getToken()
       .then((refreshedToken) => {
         localStorage.setItem("notification-token", refreshedToken);
-        postRegistrationToken({
-          userVisibleOnly: true,
-          registrationToken: refreshedToken,
-        });
       })
       .catch((err) => {
         console.log("Unable to retrieve refreshed token ", err);
       });
   });
 };
-
 export const registerSW = () => {
   if ("serviceWorker" in navigator && "PushManager" in window) {
     if (!localStorage.getItem("notification-token")) {
@@ -49,14 +39,19 @@ interface PayloadInterface {
     title: string;
     body: string;
     icon: string;
+    click_action?: string;
   };
 }
 
 messaging.onMessage((payload: PayloadInterface) => {
-  const { title, body, icon } = payload.notification;
+  const { title, body, icon, click_action } = payload.notification;
   const options = {
     body,
     icon,
   };
-  return new Notification(title, options);
+  const notification = new Notification(title, options);
+  notification.onclick = (event) => {
+    event.preventDefault();
+    window.open(click_action, "_blank");
+  };
 });
