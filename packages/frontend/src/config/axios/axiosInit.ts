fcec15986afd3878935dfe6327f29e7f491540cb
Adamwojty <adam12573@gmail.com>
2020-08-31 19:42:10 +0200

    Merge Proteced Route (WF-121)

--- packages/frontend/src/config/axios/axiosInit.ts
+++ packages/frontend/src/config/axios/axiosInit.ts
@@ -1,6 +1,20 @@
-import AxiosInstance from '@project/common/features/config/axios/configAxios';
-
-export  function axiosInit() {
-    AxiosInstance.defaults.baseURL =  process.env?.REACT_APP_API_URL;
-    AxiosInstance.defaults.headers.Authorization =  `Bearer ${localStorage.getItem("token")}`;
-};
+import { AxiosResponse, AxiosError } from "axios";
+import AxiosInstance from "@project/common/features/config/axios/configAxios";
+import { Navigation } from "../routing/NavigationPath";
+import { ServerStatus } from "./models";
+export function axiosInit() {
+  AxiosInstance.defaults.baseURL = process.env?.REACT_APP_API_URL;
+  AxiosInstance.defaults.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+  AxiosInstance.interceptors.response.use(
+    (response: AxiosResponse) => {
+      return response;
+    },
+    (error: AxiosError) => {
+      if (error.response?.status === ServerStatus.UNAUTHORIZED && window.location.pathname !== Navigation.LOGIN) {
+        sessionStorage.removeItem("token");
+        return window.location.replace(Navigation.LOGIN);
+      }
+      return Promise.reject(error);
+    }
+  );
+}
