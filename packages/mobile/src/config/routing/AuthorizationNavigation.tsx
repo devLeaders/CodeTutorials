8d30a58ad1fb5f522ea4b5bec8e5e1887cb33f28
mbobas <michal.gulczynski88@gmail.com>
2020-09-06 19:26:27 +0200

    quality

--- packages/mobile/src/config/routing/AuthorizationNavigation.tsx
+++ packages/mobile/src/config/routing/AuthorizationNavigation.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationName } from './NavigationName';
-
 import SignInScreen from '../../features/auth/page/SignInScrren';
 import SignUpScreen from '../../features/auth/page/SingUpScrenn';
 import { NavOption } from '../../features/common/components/NavOption';
 import TabNavigation from './BottomNavigation/TabNavigation';
 import SingleMovie from '../../features/videos/page/SingleMovie';
 import { VideoPlayerScreen } from '../../features/videoPlayer/page/VideoPlayerScreen';
-
-import HomeScreen from '../../features/videos/page/HomeScreen';
-import MainScreenHeader from '../../features/videos/components/MainScreen/MainScreenHeader';
-import SquareToRenderTemp from '../../features/videos/components/MainScreen/SquareToRenderTemp';
-import MoviesList from '../../features/videos/page/MoviesList';
-import FiltersScreen from '../../features/videos/page/FiltersScreen';
-import ShortcutToFilters from '../../features/videos/components/FiltersScreen/ShortcutToFilters';
-import FilterWithList from '../../features/videos/page/FilterWithList';
 
 const AuthStack = createStackNavigator();
 
