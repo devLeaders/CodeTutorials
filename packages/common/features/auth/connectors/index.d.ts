51e31bd062c74e8027f538b49ea5d12656dffb5f
Bartlomiej Zimny <b.zimny@getprintbox.com>
2020-09-23 23:33:45 +0200

    c

--- packages/common/features/auth/connectors/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { AxiosPromise } from "axios";
-import { ISingInRequest, IUserRequest, ISingInResponse } from "../models";
-export declare function signUp(obj: IUserRequest): AxiosPromise<any>;
-export declare function signIn(obj: ISingInRequest): AxiosPromise<ISingInResponse>;
